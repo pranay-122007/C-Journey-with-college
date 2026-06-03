c3632fa43420b23f6d65bd5145d331ab5859de20
Pranay Talware <pranaytalware7@gmail.com>
2026-06-03 22:15:05 +0530

    Add program to sum two numbers in Add.c

---
+++ Lecture 02/Add.c
@@ -0,0 +1,10 @@
+//Program to add two numbers
+#include<stdio.h>
+void main()
+{
+	float a,b,sum;
+	printf("Enter the value of a and b");
+    scanf("%f%f",&a,&b);
+    sum = a+b;
+    printf("Addition of two nos:%f",sum);
+}
