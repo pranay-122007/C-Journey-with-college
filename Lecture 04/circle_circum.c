c4b5648d99f79ff4fb53148102d437ed95b76982
Pranay Talware <pranaytalware7@gmail.com>
2026-06-05 09:51:27 +0530

    Create circle_circum.c

---
+++ Lecture 04/circle_circum.c
@@ -0,0 +1,14 @@
+// To find the circumference of a circle
+
+#include<stdio.h>
+void main()
+{
+    float r, circum;
+    printf("Enter the radius of circle: ");
+    scanf("%f", &r);
+    
+    // Formula: 2 * pi * r
+    circum = 2 * 3.14159 * r;
+    
+    printf("Circumference of circle = %.2f\n", circum);
+}
