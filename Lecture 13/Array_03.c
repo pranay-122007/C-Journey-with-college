8e818eb62ec8a3c1ac817492f93ddebbe6f50515
Pranay Talware <pranaytalware7@gmail.com>
2026-06-22 21:19:42 +0530

    Initialize array and correct loop boundaries
    Fix array initialization and loop conditions.

---
+++ Lecture 13/Array_03.c
@@ -0,0 +1,20 @@
+//Array : Intilizing :
+
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	printf("Enter the number of element: ");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the elements of array: ");
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Display the element of array\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+}
