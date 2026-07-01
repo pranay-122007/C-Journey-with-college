6d4f26950eadc723cf087e7e81b9751440cb353e
Pranay Talware <pranaytalware7@gmail.com>
2026-07-01 22:14:09 +0530

    To demonstrate string input/output

---
+++ Lecture 17/String_01.c
@@ -0,0 +1,11 @@
+//string inp or opt;
+#include<stdio.h>
+int main()
+{
+	char str[20];
+	printf("enter the string: ");
+	//scanf("%s",str);
+	gets(str);//this is used only in string
+	//printf("string are :%s\n",str);
+	puts(str);
+}
